7533f36987cde09397cf503467d47be97d73aede
major2000 <majormadobe@gmail.com>
2022-12-12 11:44:02 +0300

    Activate Burp Suite Professional

--- Windows_Setup.ps1
+++ Windows_Setup.ps1
@@ -77,3 +77,12 @@ if (Test-Path Burp-Suite-Pro.vbs) {
  # Remove Additional files
 rm Kali_Linux_Setup.sh
 del -Recurse -Force .\.github\
+
+
+# Activate Burp Suite Professional with keygenerator and Keyloader
+echo "Reloading Environment Variables ...."
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+echo "`n`nStarting Keygenerator ...."
+start-process java.exe -argumentlist "-jar keygen.jar"
+echo "`n`nStarting Burp Suite Professional"
+java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -javaagent:"loader.jar" -noverify -jar "Burp-Suite-Pro.jar"
